0d8c2c6f043a32aff6a0f1635bc898351fa2c492
root <root@scanner.physics.indiana.edu>
2017-08-29 08:45:35 -0400

    Use full paths for system binaries.

--- scanner.sh
+++ scanner.sh
@@ -5,6 +5,19 @@
 
 write_file='/dev/null'
 
+MKTEMP=/bin/mktemp
+ARPING=/sbin/arping
+SED=/bin/sed
+TR=/bin/tr
+CUT=/bin/cut
+DIG=/bin/dig
+GREP=/bin/grep
+TEE=/bin/tee
+SORT=/bin/sort
+CP=/bin/cp
+RM=/bin/rm
+DATE=/bin/date
+
 while getopts ":a:o:r:w:x:X:" opt; do
 	case $opt in
 		a)	asset_file="$OPTARG" ;;
@@ -22,29 +35,30 @@ done
 [[ -f "$oui_file" ]]						|| { >&2 echo '<oui_file> does not exist.'		; exit 1 ; }
 [[ -f "$write_file" ]]						&& { >&2 echo '<write_file> already exists.'	; exit 1 ; }
 
-tmpfile=$(mktemp /tmp/scanner.XXXXXXXXXX) || { >&2 echo "Could not create temporary file" ; exit 1 ; }
+tmpfile=$($MKTEMP /tmp/scanner.XXXXXXXXXX) || { >&2 echo "Could not create temporary file" ; exit 1 ; }
 
 while read ip ; do
-	if output=$(arping -f -w 2 $ip) ; then
-		mac=$(sed -n 's:.*\[\(.*\)].*:\1:p' <<< $output)
-		oui=$(tr -d : <<< $mac | cut -c 1-6)
-		hostname="$(dig +short -x $ip | sed 's/.$/,/' | tr -d '\n' | sed 's/,$//' )"
+	if output=$($ARPING -f -w 2 $ip) ; then
+		mac=$($SED -n 's:.*\[\(.*\)].*:\1:p' <<< $output)
+		oui=$($TR -d : <<< $mac | $CUT -c 1-6)
+		hostname="$($DIG +short -x $ip | $SED 's/.$/,/' | $TR -d '\n' | $SED 's/,$//' )"
 		[[ -z "$hostname" ]] && hostname="No_Hostname"
-		grep -qi $oui <<< "$exclude_oui" && oui_excluded="y" || oui_excluded="n"
-		if  [[ ! -z "$exclude_macs_file" ]] && grep -qi $mac "$exclude_macs_file" ; then
+		$GREP -qi $oui <<< "$exclude_oui" && oui_excluded="y" || oui_excluded="n"
+		if  [[ ! -z "$exclude_macs_file" ]] && $GREP -qi $mac "$exclude_macs_file" ; then
 			mac_excluded="y"
 		else
 			mac_excluded="n"
 		fi
-		[[ -f "$oui_file" ]] && vendor=$(grep $oui "$oui_file" | cut -c 8-100 | tr -s ' ' | tr ' ' '_') || vendor=''
+		[[ -f "$oui_file" ]] && vendor=$($GREP $oui "$oui_file" | $CUT -c 8-100 | $TR -s ' ' | $TR ' ' '_') || vendor=''
 		[[ -z "${vendor// }" ]] && vendor='Unknown_Vendor'
-		grep -qi $mac "$asset_file" && presence="Present" || presence="Absent"
+		$GREP -qi $mac "$asset_file" && presence="Present" || presence="Absent"
 
-		printf "%s\t%s\t%s\t%s\tOUI_Excluded:%s\tMAC_Excluded:%s\t(%s)\t%s\n" $ip $mac $(date -Is) $presence $oui_excluded $mac_excluded "$vendor" "$hostname" | tee -a "$tmpfile"
+		printf "%s\t%s\t%s\t%s\tOUI_Excluded:%s\tMAC_Excluded:%s\t(%s)\t%s\n" \
+			$ip $mac $($DATE -Is) $presence $oui_excluded $mac_excluded "$vendor" "$hostname" | $TEE -a "$tmpfile"
 	fi	
-done <<< "$(tr ' ' '\n' <<< $(eval echo $ip_range))"
+done <<< "$($TR ' ' '\n' <<< $(eval echo $ip_range))"
 
-sort -Vo "$tmpfile" "$tmpfile"
-cp "$tmpfile" "$write_file"
+$SORT -Vo "$tmpfile" "$tmpfile"
+$CP "$tmpfile" "$write_file"
 
-rm "$tmpfile"
+$RM "$tmpfile"
